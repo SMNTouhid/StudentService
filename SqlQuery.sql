f8f52d645cf60c55460d49db59ad385239374e39
Nasimuzzaman Touhid <nasimuzzamant@outlook.com>
2021-08-06 03:46:05 +0600

    sqlQuer add, clean solution

---
+++ SqlQuery.sql
@@ -0,0 +1,18 @@
+﻿create database Student
+
+use Student
+
+create table StudentInfo 
+(
+Id int identity primary key ,
+FirstName nvarchar(50),
+LastName nvarchar(50),
+Gender nvarchar(50),
+Class int
+)
+
+insert into StudentInfo values ('t','test1','male',1)
+insert into StudentInfo values ('s','test2','male',2)
+insert into StudentInfo values ('r','test3','male',3)
+insert into StudentInfo values ('m','test4','female',4)
+insert into StudentInfo values ('n','test5','female',5)
