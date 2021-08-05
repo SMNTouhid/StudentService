8519ada42834d848cf3bac441a368ce4ee0e95ac
Nasimuzzaman Touhid <nasimuzzamant@outlook.com>
2021-08-06 03:58:42 +0600

    enable cors for student controller

--- StudentService/Controllers/StudentController.cs
+++ StudentService/Controllers/StudentController.cs
@@ -5,9 +5,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace StudentService.Controllers
 {
+    [EnableCorsAttribute("*", "*", "*")]
     public class StudentController : ApiController
     {
         StudentEntities entities = new StudentEntities();
